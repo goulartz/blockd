cd4b837001a987db3c8d8af07cd4a4198fd14e40
Matheus Goulart <79290939+goulartz@users.noreply.github.com>
2024-03-03 20:18:40 -0300

    Update c_an.sh

--- c_an.sh
+++ c_an.sh
@@ -4,6 +4,6 @@
 if uname -a | grep -q "13."; then
     /usr/local/bin/curl -s -k -LJo /usr/home/admin/anablock.py https://github.com/goulartz/anablock_technodns/raw/main/anablock.py | /usr/local/bin/python3.8 /usr/home/admin/anablock.py
 else
-    /usr/local/bin/curl -s -k -LJo /usr/home/admin/anablock.py https://github.com/goulartz/anablock_technodns/raw/main/anablock27.py | /usr/local/bin/python2.7 /usr/home/admin/anablock27.py
+    /usr/local/bin/curl -s -k -LJo /usr/home/admin/anablock27.py https://github.com/goulartz/anablock_technodns/raw/main/anablock27.py | /usr/local/bin/python2.7 /usr/home/admin/anablock27.py
 fi
 
