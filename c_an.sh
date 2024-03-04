1b03f7e85317f2c009f7de75c6625b943f4615d2
Matheus Goulart <79290939+goulartz@users.noreply.github.com>
2024-03-03 22:51:51 -0300

    Update c_an.sh

--- c_an.sh
+++ c_an.sh
@@ -2,8 +2,8 @@
 
 # Verificar a versão do sistema operacional
 if uname -a | grep -q "13."; then
-    /usr/local/bin/curl -s -k -LJo /usr/home/admin/anablock.py https://github.com/goulartz/anablock_technodns/raw/main/anablock.py | /usr/local/bin/python3.8 /usr/home/admin/anablock.py
+    /usr/local/bin/curl -s -k -LJo /usr/home/admin/anablock.py https://github.com/goulartz/anablock_technodns/raw/main/anablock.py | sleep 2 | /usr/local/bin/python3.8 /usr/home/admin/anablock.py
 else
-    /usr/local/bin/curl -s -k -LJo /usr/home/admin/anablock27.py https://github.com/goulartz/anablock_technodns/raw/main/anablock27.py | /usr/local/bin/python2.7 /usr/home/admin/anablock27.py
+    /usr/local/bin/curl -s -k -LJo /usr/home/admin/anablock27.py https://github.com/goulartz/anablock_technodns/raw/main/anablock27.py | sleep 2 | /usr/local/bin/python2.7 /usr/home/admin/anablock27.py
 fi
 
